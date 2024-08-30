bcb75ca67257b2c2fedeb286b4db2184437794ce
arslan <iamarslannasr@gmail.com>
2024-08-30 22:12:17 +0500

    flavor app icon set

--- lib/constants/app_images.dart
+++ lib/constants/app_images.dart
@@ -1,4 +1,4 @@
 class AppImages {
-  static const appIcon = 'assets/icon/icon.png';
+  static const appIcon = 'assets/icon/free_flavor_app_icon.png';
   static const lottieImageLoading = 'assets/lottie/lottie.json';
 }
